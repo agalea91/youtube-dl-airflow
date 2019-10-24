fc0efd5c8a1e1e411c17a7e96177e73526f202b4
Alexander Galea <agalea91@gmail.com>
2019-10-24 08:54:10 -0700

    Fixed youtube-dl cmd from returning error code

--- dags/download_youtube_playlists.py
+++ dags/download_youtube_playlists.py
@@ -1,34 +1,39 @@
 # -*- coding: utf-8 -*-
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.dagrun_operator import TriggerDagRunOperator
-from datetime import datetime, timedelta
+from airflow.operators.dummy_operator import DummyOperator
+import datetime
 import json
 import os
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime.datetime.utcnow().date(),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
-
-dag = DAG(
-    'download_youtube_playlists',
-    default_args=default_args,
-    schedule_interval=timedelta(days=1)
-)
-
+# Set default environment & config
+os.environ['MAX_DOWNLOADS'] = os.getenv('MAX_DOWNLOADS', 10)
 PROJECT_HOME = '/usr/local/airflow/dags'
 OUTPUT_PATH = '/videos'
 YOUTUBE_DL_CMD = (
     '/usr/local/bin/youtube-dl -w -i '
     '--max-downloads {{ params.max_downloads }} '
-    '--write-info-json {{ params.playlist_url }} '
-    '-o {{ params.output }}'
+    '--write-info-json "{{ params.playlist_url }}" '
+    '-o "{{ params.output }}" || true'
+)
+
+# Set up the DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.datetime.strptime(
+        datetime.datetime.utcnow().date().isoformat(),
+        '%Y-%m-%d'
+    ) - datetime.timedelta(days=1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': datetime.timedelta(minutes=5),
+}
+dag = DAG(
+    'download_youtube_playlists',
+    default_args=default_args,
+    schedule_interval=datetime.timedelta(days=1)
 )
 
 # Load youtube playlist info
@@ -36,26 +41,28 @@ with open(os.path.join(PROJECT_HOME, 'youtube_playlists.json'), 'r') as f:
     playlists = json.loads(f.read())
 
 # Set up DAG components
+trigger_task = DummyOperator(
+    task_id='trigger',
+    dag=dag
+)
+tasks = []
 for playlist in playlists:
     output_folder = os.path.join(OUTPUT_PATH, playlist['name'])
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-    tasks = [
+    tasks.append(
         BashOperator(
             task_id=playlist['name'],
             bash_command=YOUTUBE_DL_CMD,
             params=dict(
                 path=PROJECT_HOME,
-                max_downloads=os.getenv('MAX_DOWNLOADS', 10),
+                max_downloads=int(os.getenv('MAX_DOWNLOADS')),
                 playlist_url=playlist['url'],
                 output=os.path.join(output_folder, '%(title)s-%(id)s.%(ext)s'),
             ),
             dag=dag
         )
-    ]
-
-trigger_task = TriggerDagRunOperator('download_youtube_playlists')
+    )
 
 # Build the DAG
-for task in tasks:
-    trigger_task >> task
+trigger_task >> tasks
